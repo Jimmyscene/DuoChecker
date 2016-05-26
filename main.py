d508e7df96faba0450d75ed60dd251088fe80366
Blake Easley <thejimmyscene@gmail.com>
2016-05-25 19:08:23 -0500

    I ONCE TOOK A COMPUTER SCIENCE CLASS. Changed to OOP

--- main.py
+++ main.py
@@ -1,81 +1,101 @@
 import requests
 import time
 import json
+import sys
+import pdb
 from datetime import datetime
+
+
 KEY  = "8bf96ba6-e94d-43fc-a892-06eac23bae66"
 
+class DuoChecker:
+	def __init__(self,summonerName,duoName):
+		self.summonerName = summonerName
+		self.duoName = duoName
+		self.getSummonerId()
+		self.season = None
+		self.calculate()
 
-def getSummonerId(summonerName):
-	url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summonerName + "?api_key=" + KEY
-	r = requests.get(url)
-	return r.json()
-
-def getMatchHistory(summonerId,season=None):
-	url = "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/"+ summonerId +"?rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5&seasons=SEASON2016" + "&endTime=" + "1464058733754" "&api_key=" + KEY
-	r = requests.get(url)
-	return r.json()
-
-def getMatchResults(matchId,summonerName="JimmyScene",duoName="Camel Jesus"):
-	url = "https://na.api.pvp.net/api/lol/na/v2.2/match/"+ matchId + "?api_key=8bf96ba6-e94d-43fc-a892-06eac23bae66"
-	r = requests.get(url)
-	results = r.text
-	results = json.loads(results)
-
-	try:
-		# print(datetime.fromtimestamp(results["matchCreation"] /1000).ctime())
-		myId = None
-		withDuo = False
-		for x in results["participantIdentities"]:
-			if(x["player"]["summonerName"] == summonerName):
-				myId=x["participantId"]
-			if(x["player"]["summonerName"] == duoName):
-				withDuo = True
-		
-		if(myId==None):
-			return -1
-		else:
-			result = results["teams"][0]["winner"]
-			winner = result if myId<6 else not result 
+	def calculate(self):
+		matchHistory = self.getMatchHistory()
+		try:
+			total = matchHistory["totalGames"]
+		except Exception as e:
+			sys.exit("Errored")
+		winsDuo,gamesDuo, winsSolo, gamesSolo = 0,0,0,0
+		for index,match in enumerate(matchHistory["matches"]):
+			sys.stdout.flush()
+			sys.stdout.write("\r%d%%" % (index*100/total))
+			match=str(match["matchId"])
+			result = self.getMatchResults(match)
+			if(result !=-1):
+				if(result["withDuo"]):
+					gamesDuo+=1
+					if(result["won"]):
+						winsDuo+=1
+				else:
+					gamesSolo+=1
+					if(result["won"]):
+						winsSolo+=1
 			
-			return {
-			"won" : winner ,
-			"withDuo": withDuo
-			}
-	except KeyError:
-		if(results["status"]["status_code"]==429):
-			time.sleep(8)
-			return getMatchResults(matchId)
-		elif(results["status"]["status_code"]==404):
-			return {"error": 404, "url": url}
+		print()
+		print("Duo Winrate: %d%%" % (winsDuo*100/gamesDuo) )
+		print("Solo Winrate: %d%%" % (winsSolo*100/gamesSolo)  )
+		print("With Duo:")
+		print("Won: %s Out of %s " %(winsDuo,gamesDuo))
+		print("Solo: ")
+		print("Won: %s Out of %s " %(winsSolo,gamesSolo))
+			
+
+	def getSummonerId(self):
+		url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/%s?api_key=%s"% (self.summonerName.lower(),KEY)
+		response = requests.get(url)
+		self.summonerId = response.json()[self.summonerName.lower()]["id"]
+
+	def getMatchHistory(self):
+		now = 1464058733754 #int(round(time.time()))
+		url = "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/%s?rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5&seasons=SEASON2016&endTime=%d&api_key=%s" % (self.summonerId,now,KEY)
+		response = requests.get(url)
+		return response.json()
+
+	def getMatchResults(self,matchId):
+		url = "https://na.api.pvp.net/api/lol/na/v2.2/match/%s?api_key=%s" % (matchId,KEY)
+		response = requests.get(url)
+		results = response.text
+		results = json.loads(results)
+		try:
+			myId = None
+			withDuo = False
+			for x in results["participantIdentities"]:
+				if(x["player"]["summonerName"] == self.summonerName):
+					myId=x["participantId"]
+				if(x["player"]["summonerName"] == self.duoName):
+					withDuo = True
+			
+			if(myId==None):
+				return -1
+			else:
+				result = results["teams"][0]["winner"]
+				winner = result if myId<6 else not result 
+				
+				return {
+				"won" : winner ,
+				"withDuo": withDuo
+				}
+		except KeyError as e:
+			status_code = results["status"]["status_code"]
+			if(status_code in {429,500} ):
+				time.sleep(4)
+				return self.getMatchResults(matchId)
+			elif(status_code == 404):
+				return {"error": 404, "url": url}
+			else:
+				print(results)
 
 
 
 if __name__ == '__main__':
-	summoner = "Jimmyscene".lower()	
-	x= str(getSummonerId(summoner)[summoner]["id"])
-	matchHistory = getMatchHistory(x)
-	total = matchHistory["totalGames"]
-	winsDuo = 0
-	gamesDuo =0
-	winsSolo = 0
-	gamesSolo =0
-	index=0
-	for match in matchHistory["matches"]:
-		print(str(index) + " /" + str(total))
-		match=str(match["matchId"])
-		result = getMatchResults(match)
-		if(result !=None):
-			if(result["withDuo"]):
-				gamesDuo+=1
-				if(result["won"]):
-					winsDuo+=1
-			else:
-				gamesSolo+=1
-				if(result["won"]):
-					winsSolo+=1
-			index+=1
-	print("With Duo:")
-	print("Won: " + str(winsDuo) + "Out of " + str(gamesDuo) )
-	print("Solo: ")
-	print("Won: " + str(winsSolo) + "Out of " + str(gamesSolo) )
+	DuoChecker("JimmyScene","Camel Jesus")
+	
 
+	
